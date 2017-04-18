ac841823ec3f884a446f4f8b620ae4aba36ca382
Jeffrey Chen <jeffrey@phd.com.tw>
2017-04-18 14:11:26 +0800

    Initial Commit

---
+++ GithubTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubTest2
+//
+//  Created by Jeffrey Chen on 2017/4/18.
+//  Copyright © 2017年 Jeffrey Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
